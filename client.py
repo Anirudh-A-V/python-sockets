2973669accd92ff302ccd83c464fd11e545b2a52
Anirudh A V <anirudh.av02@gmail.com>
2022-09-12 18:26:49 +0530

    comments aligned

--- client.py
+++ client.py
@@ -8,30 +8,25 @@ FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!EXIT"
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        # create a socket object
+client.connect(ADDR)                                              # connect to the server
 
-client.connect(ADDR) # connect to the server
-
-# print("Connected to server")
+print("Connected to server")
 
 def send(msg):
-    message = msg.encode(FORMAT) # encode the message
-    msg_length = len(message) # get the length of the message
+    message = msg.encode(FORMAT)                                  # encode the message
+    msg_length = len(message)                                     # get the length of the message
+    send_length = str(msg_length).encode(FORMAT)                  # encode the length of the message
+    send_length += b' ' * (HEADER - len(send_length))             # add spaces to the end of the message to make it 64 bytes long
+    client.send(send_length)                                      # send the length of the message
+    client.send(message)                                          # send the message
     
-    send_length = str(msg_length).encode(FORMAT) # encode the length of the message
-    send_length += b' ' * (HEADER - len(send_length)) # add spaces to the end of the message to make it 64 bytes long
-    
-    client.send(send_length) # send the length of the message
-    client.send(message) # send the message
-    
-    print(client.recv(2048).decode(FORMAT)) # receive the message from the server
+    print(client.recv(2048).decode(FORMAT))                       # receive the message from the server
     
 while True:
     string = input(f"IP {socket.gethostbyname(socket.gethostname())} : ")
-    # str = str.str()
     if string == DISCONNECT_MESSAGE:
         break
     send(string)
 
-# send("Hello World!")
 send(DISCONNECT_MESSAGE)
--- server.py
+++ server.py
@@ -2,14 +2,13 @@ import socket
 import threading
 
 PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())  # IP address of the local server
+SERVER = socket.gethostbyname(socket.gethostname())                          # IP address of the local server
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!EXIT"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket object
-
-server.bind(ADDR) # bind the socket to the address
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                   # create a socket object
+server.bind(ADDR)                                                            # bind the socket to the address
 
 
 def handle_client(conn, addr):
@@ -17,27 +16,27 @@ def handle_client(conn, addr):
     
     connected = True
     while connected:
-        msg_length = conn.recv(64).decode(FORMAT) # receive the length of the message
+        msg_length = conn.recv(64).decode(FORMAT)                            # receive the length of the message
         if msg_length:
             msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT) # receive the message
+            msg = conn.recv(msg_length).decode(FORMAT)                       # receive the message
             if msg == DISCONNECT_MESSAGE:
                 connected = False
                 
             print(f"[{addr}] {msg}")
             conn.send("Msg received".encode(FORMAT))
     
-    conn.close() # close the connection
+    conn.close()                                                             # close the connection
         
 
 def start():
-    server.listen() # start listening for connections
+    server.listen()                                                          # start listening for connections
     print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
         conn, addr = server.accept() # accept a connection
-        thread = threading.Thread(target=handle_client, args=(conn, addr)) # create a thread for each client
+        thread = threading.Thread(target=handle_client, args=(conn, addr))   # create a thread for each client
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}") # print the number of active connections
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")        # print the number of active connections
 
 print("[STARTING] server is starting...")
 start()
